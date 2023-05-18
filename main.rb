4a07ffc883c53a64117b3a56b8c61f0e4d955c9f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-05-18 19:12:25 +0530

    Fix linter errors

--- main.rb
+++ main.rb
@@ -3,18 +3,20 @@ require_relative 'List'
 list = List.new(1, 2, 3, 4)
 
 # Test #all?
-puts list.all? { |e| e < 5 }
+puts(list.all? { |e| e < 5 })
 # Output: true
-puts list.all? { |e| e > 5 }
+puts(list.all? { |e| e > 5 })
 # Output: false
 
 # Test #any?
-puts list.any? { |e| e == 2 }
+puts(list.any? { |e| e == 2 })
 # Output: true
-puts list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 5 })
 # Output: false
 
 # Test #filter
-puts list.filter { |e| e.even? }
+print(list.filter(&:even?))
 # Output: [2, 4]
-# When I run this code it gives me this error "undefined local variable or method `enum' for MyList:Class (NameError)" how can I fix it?
+puts # Newline
+print(list.filter(&:odd?))
+# Output: [1, 3]
